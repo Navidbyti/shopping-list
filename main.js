608bfa8b5390ed20790c692dfc13251482c8cde3
Navid <nbayati22@gmail.com>
2021-06-07 00:19:17 +0430

    pretty much finished i'll add some css later maybe

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-//Writing stuff so i can push to github
+const shopList = document.querySelector('.shopping-items > ul');
+const submitBtn = document.querySelector('.submit-btn');
+const shopInput = document.querySelector('.text-input');
+shopInput.focus();
+
+function clickEvent() {
+if(!shopInput.value){
+    alert('Please enter a shoping item!');
+    shopInput.focus();
+    return;
+}
+createItem(shopInput.value);
+shopInput.value = '';
+shopInput.focus();
+
+
+}
+
+submitBtn.addEventListener('click', clickEvent);
+
+
+function createItem(input) {
+const listItem = document.createElement('li');
+const itemDltBtn = document.createElement('button');
+const itemName = document.createTextNode(input);
+
+itemDltBtn.textContent= 'Delete';
+listItem.append(itemName , itemDltBtn);
+shopList.appendChild(listItem);
+
+itemDltBtn.addEventListener('click', ()=>{
+    shopList.removeChild(listItem);
+    shopInput.focus();
+
+})
+
+}
+createItem('Test Item');
